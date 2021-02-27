0eaada5396850b81fa14f883f7b040c74b2f21c8
DaBluLite <dabluliteplays@gmail.com>
2021-02-27 15:15:44 +0200

    Update lunar-installer.sh

--- lunar-installer.sh
+++ lunar-installer.sh
@@ -1,16 +1,16 @@
 sudo mkdir /opt/lunar-client
-cd /opt/lunar-client
+sudo cd /opt/lunar-client
 sudo wget -r -l 1 --span-hosts --output-document=lunar.AppImage --accept-regex='https://launcherupdates.lunarclientcdn.com/Lunar%20Client-*.*.*.AppImage' -erobots=off -nH https://api.lunarclientprod.com/site/download?os=linux
 sudo chmod +xwr lunar.AppImage
 sudo ./lunar.AppImage --appimage-extract
-cd squashfs-root
+sudo cd squashfs-root
 sudo cp * /opt/lunar-client -r
 sudo rm * -r
-cd /opt/lunar-client
+sudo cd /opt/lunar-client
 sudo rm squashfs-root -r
-cd /home/*/Desktop
-echo -n > lunarclient.desktop
-cat <<EOT >> lunarclient.desktop
+sudo cd /home/*/Desktop
+sudo echo -n > lunarclient.desktop
+sudo cat <<EOT >> lunarclient.desktop
 [Desktop Entry]
 Name=Lunar Client
 Exec=/opt/lunar-client/AppRun
@@ -22,13 +22,13 @@ X-AppImage-Version=2.5.0
 Comment=Lunar Client
 Categories=Game;
 EOT
-chmod 775 /home/*/Desktop/lunarclient.desktop
-chmod +x /home/*/Desktop/lunarclient.desktop
-cd /opt/lunar-client
-chmod 775 /opt/lunar-client -R
-chmod 775 /opt/lunar-client/AppRun
-chmod +rwx /opt/lunar-client/AppRun
-chmod 775 /opt/lunar-client/lunarclient
-chmod +rwx /opt/lunar-client/lunarclient
-cd /home/*/Desktop
+sudo chmod 775 /home/*/Desktop/lunarclient.desktop
+sudo chmod +x /home/*/Desktop/lunarclient.desktop
+sudo cd /opt/lunar-client
+sudo chmod 775 /opt/lunar-client -R
+sudo chmod 775 /opt/lunar-client/AppRun
+sudo chmod +rwx /opt/lunar-client/AppRun
+sudo chmod 775 /opt/lunar-client/lunarclient
+sudo chmod +rwx /opt/lunar-client/lunarclient
+sudo cd /home/*/Desktop
 sudo cp lunarclient.desktop /usr/share/applications
