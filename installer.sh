a5930138dcfca89e07c5dc64ca16dc01017b84bb
DaBluLite <dabluliteplays@gmail.com>
2021-03-09 19:18:03 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,57 +1,55 @@
+clear
+echo -en "\033]0;Lunar Client Linux Installer\a"
+APP_NAME='Lunar Client' #This will be the main app name. It will be displayed in the app launcher and in the desktop shortcut
+APP_PATH_NAME=lunar-client #This will be the name for the paths, the AppImage and others. MUST BE URL-FRIENDLY!
+APP_URL=https://launcherupdates.lunarclientcdn.com/Lunar%20Client-2.5.2.AppImage #This must lead to an appimage file
 SUDO_USER=$(logname)
-mkdir /opt/lunar-client
-cd /opt/lunar-client
-wget -r -l 1 --span-hosts --output-document=lunar.AppImage --accept-regex='https://launcherupdates.lunarclientcdn.com/Lunar%20Client-*.*.*.AppImage' -erobots=off -nH https://api.lunarclientprod.com/site/download?os=linux
-chmod +xwr lunar.AppImage
-chown $SUDO_USER lunar.AppImage
-./lunar.AppImage --appimage-extract
-cd /opt/lunar-client/squashfs-root
-cp * /opt/lunar-client -r
-rm * -r
-cd /opt/lunar-client
-rm squashfs-root -r
-cd /home/*/Desktop
-echo -n > lunarclient.desktop
-cat <<EOT >> lunarclient.desktop
-[Desktop Entry]
-Name=Lunar Client
-Exec=/opt/lunar-client/AppRun
-Terminal=false
-Type=Application
-Icon=/opt/lunar-client/lunarclient.png
-StartupWMClass=Lunar Client
-X-AppImage-Version=2.5.0
-Comment=Lunar Client
-Categories=Game;
+echo 'Available options:'
+echo '1) Install '$APP_NAME
+echo '2) Uninstall '$APP_NAME
+echo -n 'Choose an option: ' && read INSTALLER_OPTION
+if [[ $INSTALLER_OPTION = 1 ]]
+then
+  mkdir /opt/$APP_PATH_NAME
+  cd /opt/$APP_PATH_NAME
+  wget -r -l 1 --span-hosts --output-document=$APP_PATH_NAME.AppImage -erobots=off -nH $APP_URL
+  chmod +xwr $APP_PATH_NAME.AppImage
+  chown $SUDO_USER $APP_PATH_NAME.AppImage
+  ./$APP_PATH_NAME.AppImage --appimage-extract
+  cd /opt/$APP_PATH_NAME/squashfs-root
+  cp .DirIcon /opt/$APP_PATH_NAME
+  cp * /opt/$APP_PATH_NAME -r
+  rm * -r
+  cd /opt/$APP_PATH_NAME
+  rm squashfs-root -r
+  cd /home/*/Desktop
+  echo -n > $APP_PATH_NAME.desktop
+  cat <<EOT >> $APP_PATH_NAME.desktop
+  [Desktop Entry]
+  Name=$APP_NAME
+  Exec=/opt/$APP_PATH_NAME/AppRun
+  Terminal=false
+  Type=Application
+  Icon=/opt/$APP_PATH_NAME/.DirIcon
+  StartupWMClass=$APP_NAME
+  X-AppImage-Version=2.5.0
+  Comment=$APP_NAME
+  Categories=Utility;
 EOT
-chmod 775 /home/*/Desktop/lunarclient.desktop
-chmod +x /home/*/Desktop/lunarclient.desktop
-chown $SUDO_USER /home/*/Desktop/lunarclient.desktop
-cd /opt/lunar-client
-echo -n > lunar-client.sh
-cat <<EOT >> lunar-client.sh
-function lunar-client() {
-  
-set +e
-bash -e <<TRY
-  bash /opt/lunar-client/AppRu &> /dev/null
-TRY
-if [ $? -ne 0 ]; then
-  echo "Lunar Client is not installed. If you want to install it, go to 'https://github.com/DaBluLite/lunar-client-linux-installer' for the instructions."
-  echo "If you see this even after uninstalling Lunar Client, go to 'https://github.com/DaBluLite/lunar-client-linux-installer/blob/main/remove-bash-command.md' to see how to remove it."
+  chmod 775 /home/*/Desktop/$APP_PATH_NAME.desktop
+  chmod +x /home/*/Desktop/$APP_PATH_NAME.desktop
+  chown $SUDO_USER /home/*/Desktop/$APP_PATH_NAME.desktop
+  cd /opt/$APP_PATH_NAME
+  chmod +xwr /opt/$APP_PATH_NAME -R
+  chmod 775 /opt/$APP_PATH_NAME -R
+  chown $SUDO_USER /opt/$APP_PATH_NAME -R
+  chmod 775 /opt/$APP_PATH_NAME/AppRun
+  chmod +rwx /opt/$APP_PATH_NAME/AppRun
+  chown $SUDO_USER /opt/$APP_PATH_NAME/AppRun
+  cd /home/*/Desktop
+  cp $APP_PATH_NAME.desktop /usr/share/applications
+else
+  rm -R /opt/$APP_PATH_NAME
+  rm /home/*/Desktop/$APP_PATH_NAME.desktop
+  rm /usr/share/applications/$APP_PATH_NAME.desktop
 fi
-}
-EOT
-chmod 775 /opt/lunar-client/lunar-client.sh
-chmod +rwx /opt/lunar-client/lunar-client.sh
-chown $SUDO_USER /opt/lunar-client/lunar-client.sh
-chmod 775 /opt/lunar-client -R
-chown $SUDO_USER /opt/lunar-client -R
-chmod 775 /opt/lunar-client/AppRun
-chmod +rwx /opt/lunar-client/AppRun
-chown $SUDO_USER /opt/lunar-client/AppRun
-chmod 775 /opt/lunar-client/lunarclient
-chmod +rwx /opt/lunar-client/lunarclient
-chown $SUDO_USER /opt/lunar-client/lunarclient
-cd /home/*/Desktop
-cp lunarclient.desktop /usr/share/applications
