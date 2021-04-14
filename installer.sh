4e59a24cefde1755802eb8cb8017b9d447944ef6
DaBluLite <dabluliteplays@gmail.com>
2021-04-14 13:17:56 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -48,8 +48,10 @@ EOT
   chown $SUDO_USER /opt/$APP_PATH_NAME/AppRun
   cd /home/*/Desktop
   cp $APP_PATH_NAME.desktop /usr/share/applications
+  ln -s /opt/$APP_PATH_NAME/lunarclient /bin/$APP_PATH_NAME
 else
   rm -R /opt/$APP_PATH_NAME
   rm /home/*/Desktop/$APP_PATH_NAME.desktop
   rm /usr/share/applications/$APP_PATH_NAME.desktop
+  rm /bin/$APP_PATH_NAME
 fi
