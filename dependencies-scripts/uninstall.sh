350114382941d65b0d5b6da7d3b7c28c5d5ae8ed
DaBluLite <dabluliteplays@gmail.com>
2021-04-15 07:19:36 +0300

    Update uninstall.sh

--- dependencies-scripts/uninstall.sh
+++ dependencies-scripts/uninstall.sh
@@ -2,4 +2,4 @@ rm -R /opt/lunar-client
 rm /home/*/Desktop/lunar-client.desktop
 rm /usr/share/applications/lunar-client.desktop
 rm /bin/lunar-client
-rm -R /bin/lunar-client-linux-installer
+rm -R /bin/lunar-client-installer
